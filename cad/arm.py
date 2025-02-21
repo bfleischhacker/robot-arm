18ae7923b4c1df54df81b0178e67e340ce45829a
Ben F <bfly2000@gmail.com>
2025-02-21 01:34:34 -0500

    finished forearm limb v2

--- cad/arm.py
+++ cad/arm.py
@@ -20,16 +20,18 @@ UNIT_VEC = Vector(1, 1, 1)
 
 # servo_mount_screw = fastener.ButtonHeadScrew(size='M1.6-0.35', length=3 * MM)
 m3 = fastener.ButtonHeadScrew(size="M3-0.5", length=3)
+print(m3.thread_diameter, m3.thread_pitch)
 
 importer = Mesher()
-servo_wheel_hole_radius = 1.25
+servo_wheel_hole_radius = m3.thread_diameter / 2 * 1.05
 servo_mount_hole_radius = .8
-guide_axle_mount_radius = 3.02
-motor_axle_mount_radius = 1.6
+guide_axle_mount_radius = 3.02 * 1.05
+motor_axle_mount_radius = 1.6 * 1.05
 wheel_radius = 9.0
 carriage_mount_hole_rad = .8
-carriage_mount_hole_head_rad = 1.2
+carriage_mount_hole_head_rad = 1.4
 wheel_mount_hole_rad_from_center = 7
+arm_width = wheel_radius * 1.1
 
 
 @dataclass
@@ -124,14 +126,14 @@ def build_servo():
     # wheel joints
     with Locations(Location(Plane.XY.reverse(), (-25.5, 0, -5.1))) as locs:
         RigidJoint(f"axle", to_part=drive_wheel, joint_location=locs.locations[0])
-    with Locations(Location(Plane.XY.reverse(), (-25.5, 0, -9.6))) as locs:
+    with Locations(Location(Plane.XY, (-25.5, 0, -9.6))) as locs:
         with PolarLocations(7, 4) as locs:
             for i, l in enumerate(locs):
                 RigidJoint(f"mount_{i}", to_part=drive_wheel, joint_location=l)
 
     with Locations(Location(Plane.XY, (-25.5, 0, 24.3))) as locs:
         RigidJoint(f"axle", to_part=guide_wheel, joint_location=locs.locations[0])
-    with Locations(Location(Plane.XY, (-25.5, 0, 27.65))) as locs:
+    with Locations(Location(Plane.XY.reverse(), (-25.5, 0, 27.65))) as locs:
         with PolarLocations(7, 4) as locs:
             for i, l in enumerate(locs):
                 RigidJoint(f"mount_{i}", to_part=guide_wheel, joint_location=l)
@@ -253,9 +255,9 @@ def build_carriage(tol: float, thickness: float):
 
 def build_wheel_arm(length: float, thickness: float, axle_radius: float):
     with BuildPart() as arm:
-        b = Box(length, wheel_radius * 2, thickness)
+        b = Box(length, arm_width * 2, thickness)
         f = [edges(Select.ALL).sort_by(Axis.X)[-2], edges(Select.ALL).sort_by(Axis.X)[-4]]
-        fillet(f, wheel_radius - .001)
+        fillet(f, arm_width - .001)
         with Locations(Location((length/2-wheel_radius, 0, 0))):
             Hole(axle_radius, thickness)
             with PolarLocations(wheel_mount_hole_rad_from_center, 4) as locs:
@@ -281,10 +283,8 @@ def build_bicep(
         carriage_sy = guidef.faces()[0].width / 2
         motor_arm = build_wheel_arm(length, thickness, motor_axle_mount_radius)
         guide_arm = build_wheel_arm(length, thickness, guide_axle_mount_radius)
-        yoff = length / 2 - carriage_sy
-        arm_mid = -motorf.center().Y / 2 + length / 2
         with BuildPart(motorf):
-            x = -(motorf.center().Z - botz)+wheel_radius
+            x = -(motorf.center().Z - botz)+arm_width
             y = -length / 2 + motorf.bounding_box().size.Y / 2
             with Locations(Location((x, y, thickness / 2), Vector(Z=-90))) as loc:
                 add(motor_arm)
@@ -292,7 +292,7 @@ def build_bicep(
             with Locations(Location((0, 0, -thickness * 1.5))):
                 Hole(carriage_mount_hole_head_rad, thickness / 2)
         with BuildPart(guidef):
-            x = -(guidef.center().Z - botz)+wheel_radius
+            x = -(guidef.center().Z - botz)+arm_width
             y = length / 2 - guidef.bounding_box().size.Y / 2
             with Locations(Location((x, y, thickness / 2), Vector(Z=90))):
                 add(guide_arm)
@@ -301,18 +301,100 @@ def build_bicep(
                 Hole(carriage_mount_hole_head_rad, thickness / 2)
         cross_barf = faces(Select.ALL).filter_by(Axis.Y).sort_by(Axis.Y)[-3]
         with BuildPart(cross_barf):
-            with Locations(Location((-cross_barf.length / 2 + wheel_radius, 0, (length - motorf.width)/2))):
-                r = Box(wheel_radius * 2, cross_barf.width, thickness)
-
+            with Locations(Location((-cross_barf.length / 2 + arm_width, 0, (length - motorf.width)/2))):
+                r = Box(arm_width * 2, cross_barf.width, thickness)
+                # with BuildPart(motor_armf):
+            # return 
+        motor_arm_mount_holesf = faces(Select.ALL).filter_by(GeomType.CYLINDER).sort_by(Axis.X).sort_by(Axis.Y)[8:22]
+        motor_arm_mount_holesf = motor_arm_mount_holesf.sort_by(Axis.X)[7:]
+        motor_arm_mount_holesf = motor_arm_mount_holesf.sort_by(SortBy.RADIUS)[:4]
+        for i, f in enumerate(motor_arm_mount_holesf.faces()):
+            RigidJoint(f'motor_wheel_mount_{i}', bicep, Location(f.bounding_box().center() + (-thickness / 2, 0, 0), (0, -90, 0)))
+        guide_arm_mount_holesf = faces(Select.ALL).filter_by(GeomType.CYLINDER).sort_by(Axis.X).sort_by(Axis.Y)[8:22]
+        guide_arm_mount_holesf = guide_arm_mount_holesf.sort_by(Axis.X)[:7]
+        guide_arm_mount_holesf = guide_arm_mount_holesf.sort_by(SortBy.RADIUS)[:4]
+        for i, f in enumerate(guide_arm_mount_holesf.faces()):
+            RigidJoint(f'guide_wheel_mount_{i}', bicep, Location(f.bounding_box().center() + (thickness / 2, 0, 0), (0, 90, 0)))
     return bicep
     
+def build_forearm(
+    length: float = 150,
+    thickness: float = 2.5,
+    tol: float = 1.01,
+):
+    carriage = build_carriage(tol, thickness)
+    carriage_motorf = carriage.faces(Select.ALL).filter_by(GeomType.PLANE).filter_by(Axis.X).sort_by(Axis.X)[-1] 
+    carriage_guidef = carriage.faces(Select.ALL).filter_by(GeomType.PLANE).filter_by(Axis.X).sort_by(Axis.X)[0]
+    carriage_botz = carriage.faces(Select.ALL).filter_by(GeomType.PLANE).filter_by(Axis.Z).sort_by(Axis.Z)[0].center().Z
+    carriage_topz = carriage.faces(Select.ALL).filter_by(GeomType.PLANE).filter_by(Axis.Z).sort_by(Axis.Z)[-1].center().Z
+    cj_mot_bot = RigidJoint('carriage_motor_side_to_arm_mount_bot', carriage.part, Location((carriage_motorf.center().X, carriage_motorf.center().Y, carriage_botz), (0, 90, 0)))
+    cj_mot_top = RigidJoint('carriage_motor_side_to_arm_mount_top', carriage.part, Location((carriage_motorf.center().X, carriage_motorf.center().Y, carriage_topz), (0, 90, 0)))
+    cj_guide_bot = RigidJoint('carriage_guide_side_to_arm_mount_bot', carriage.part, Location((carriage_guidef.center().X, carriage_motorf.center().Y, carriage_botz), (0, 90, 0)))
+    cj_guide_top = RigidJoint('carriage_guide_side_to_arm_mount_top', carriage.part, Location((carriage_guidef.center().X, carriage_motorf.center().Y, carriage_topz), (0, 90, 0)))
+    carriage_sz = carriage_topz - carriage_botz
+    carriage_sx = carriage_motorf.center().X - carriage_guidef.center().X
+    carriage_sy = carriage_motorf.length
+    
+    with BuildPart() as motor_arm:
+        with Locations(Location((-length / 4, 0, 0), (0, 0, 180))):
+            front = build_wheel_arm(length / 2, thickness, motor_axle_mount_radius)
+            add(front)
+        with Locations(Location((length / 4, 0, 0))):
+            Box(length/2, arm_width * 2, thickness)
+    mj_bot = RigidJoint('motor_arm_carriage_side_bot_mount', motor_arm.part, Location((length / 2, 0, -thickness / 2), (0, 0, 180)))
+    mj_top = RigidJoint('motor_arm_carriage_side_top_mount', motor_arm.part, Location((length / 2 - carriage_sz, 0, -thickness / 2), (0, 0, 180)))
+
+    with BuildPart() as guide_arm:
+        with Locations(Location((-length / 4, 0, 0), (0, 0, 180))):
+            front = build_wheel_arm(length / 2, thickness, guide_axle_mount_radius)
+            add(front)
+        with Locations(Location((length / 4, 0, 0))):
+            Box(length/2, arm_width * 2, thickness)
+    gj_bot = RigidJoint('guide_arm_carriage_side_bot_mount', guide_arm.part, Location((length / 2, 0, thickness / 2), (0, 0, 180)))
+    gj_top = RigidJoint('guide_arm_carriage_side_top_mount', guide_arm.part, Location((length / 2 - carriage_sz, 0, thickness / 2), (0, 0, 180)))
+    
+    cj_mot_top.connect_to(mj_bot)
+    cj_mot_bot.connect_to(mj_top)
+    cj_guide_bot.connect_to(gj_top)
+    cj_guide_top.connect_to(gj_bot)
+    part = carriage.part + motor_arm.part + guide_arm.part
+    with BuildPart(carriage_motorf) as p:
+        with Locations(Location((0, 0, thickness/2))):
+            extrude(carriage_motorf, amount=thickness)
+    part += p.part
+    with BuildPart(carriage_guidef) as p:
+        with Locations(Location((0, 0, thickness/2))):
+            extrude(carriage_guidef, amount=thickness)
+    part += p.part
+    
+    jls = [part.joints[k].location for k in part.joints if k.startswith('motor_mount_') or k.startswith('guide_mount_')]
+    with BuildPart() as p:
+        with Locations(jls):
+            with Locations(Location((0,0,-thickness - thickness))):
+                Cylinder(carriage_mount_hole_head_rad, thickness * 1.5)
+                Cylinder(carriage_mount_hole_rad, thickness * 2)
+    part -= p.part
+
+    arm_mount_holesf = part.faces().filter_by(GeomType.CYLINDER).sort_by(Axis.X).sort_by(Axis.Y)[5:19]
+    motor_arm_mount_holesf = arm_mount_holesf.sort_by(Axis.X)[7:]
+    motor_arm_mount_holesf = motor_arm_mount_holesf.sort_by(SortBy.RADIUS)[:4]
+    for i, f in enumerate(motor_arm_mount_holesf.faces()):
+        RigidJoint(f'motor_wheel_mount_{i}', part, Location(f.bounding_box().center() + (-thickness / 2, 0, 0), (0, -90, 0)))
+    guide_arm_mount_holesf = arm_mount_holesf.sort_by(Axis.X)[:7]
+    guide_arm_mount_holesf = guide_arm_mount_holesf.sort_by(SortBy.RADIUS)[:4]
+    for i, f in enumerate(guide_arm_mount_holesf.faces()):
+        RigidJoint(f'guide_wheel_mount_{i}', part, Location(f.bounding_box().center() + (thickness / 2, 0, 0), (0, 90, 0)))
+
+    return part
+
+
 def connect_arm_to_servo_wheel(arm: Compound, servo: Compound):
     print("connecting", arm.label, "wheel to", servo.label)
     aj = joint_dict(arm)
     sj = joint_dict(servo)
-    for i in range(4):
-        sj[f"guide_wheel.mount_{i}"].connect_to(aj[f"l_arm_mount_{i}"])
-        sj[f"drive_wheel.mount_{i}"].connect_to(aj[f"r_arm_mount_{i}"])
+    for i in range(0, 4, 2):
+        sj[f"guide_wheel.mount_{i}"].connect_to(aj[f"guide_wheel_mount_{i}"])
+        sj[f"drive_wheel.mount_{i}"].connect_to(aj[f"motor_wheel_mount_{i}"])
 
 
 def connect_servo_to_arm_mount(arm: Compound, servo: Compound):
@@ -324,10 +406,10 @@ def connect_servo_to_arm_mount(arm: Compound, servo: Compound):
     aj['motor_mount_sw'].connect_to(sj['housing.motor_mount_sw'])
 
 
-def connect_wheels_to_housing(servo: Compound):
+def connect_wheels_to_housing(servo: Compound, angle: float):
     j = joint_dict(servo)
-    j["housing.guide_axle"].connect_to(j["guide_wheel.axle"], angle=0)
-    j["housing.drive_axle"].connect_to(j["drive_wheel.axle"], angle=0)
+    j["housing.guide_axle"].connect_to(j["guide_wheel.axle"], angle=angle)
+    j["housing.drive_axle"].connect_to(j["drive_wheel.axle"], angle=angle)
 
 
 def build_full_arm():
@@ -336,49 +418,35 @@ def build_full_arm():
     for i in range(1, 3):
         servos.append(copy.copy(servos[0]))
         servos[i].label = f"servo_{i}"
-    bicep = build_bicep(carriage_offset=6).part
+    bicep = build_bicep(150, 2.5, 1.01).part
     bicep.label = "bicep"
-    forearm = build_limb(carriage_offset=6, mirrored=True).part
+    forearm = build_forearm(150, 2.5, 1.01)
     forearm.label = "forearm"
-    connect_wheels_to_housing(servos[0])
+    servos[0].rotate(Axis.Y, -90)
+    connect_wheels_to_housing(servos[0], 180)
     connect_arm_to_servo_wheel(bicep, servos[0])
     connect_servo_to_arm_mount(bicep, servos[1])
-    connect_wheels_to_housing(servos[1])
+    connect_wheels_to_housing(servos[1], angle=90)
     connect_arm_to_servo_wheel(forearm, servos[1])
     connect_servo_to_arm_mount(forearm, servos[2])
-    connect_wheels_to_housing(servos[2])
-    export_step(bicep, "bicep.step")
-    export_step(forearm, "forearm.step")
+    connect_wheels_to_housing(servos[2], 0)
+    # export_step(forearm, "forearm.step")
     return Compound(label="arm", children=[bicep, forearm] + servos)
-
-def build_test_arm():
-    servo = build_servo()
-    servo.label = 'servo'
-    servo2 = copy.copy(servo)
-    servo2.label = 'servo2'
-    limb = build_limb(mirrored=True).part
-    limb.label = 'limb'
-    connect_arm_to_servo_wheel(limb, servo)
-    connect_wheels_to_housing(servo)
-    connect_servo_to_arm_mount(limb, servo2)
-    # connect_wheels_to_housing(servo2)
-    return Compound(label='bicep', children=[servo, servo2, limb])
-
-
 
 
 # arm = build_full_arm()
 # show(build_servo(), render_joints=True)
 # show(build_full_arm(), render_joints=True)
-servo = build_servo()
+# servo = build_servo()
 # carriage = build_carriage(1.01, 2.0)
-limb = build_bicep().part
+# limb = build_bicep().part
 # limb.joints['motor_mount_se'].connect_to(get_descendant(servo, 'housing').joints['motor_mount_se'])
 # limb.joints['motor_mount_sw'].connect_to(get_descendant(servo, 'housing').joints['motor_mount_sw'])
-connect_servo_to_arm_mount(limb, servo)
-connect_wheels_to_housing(servo)
-show(limb, servo, render_joints=True)
+# connect_servo_to_arm_mount(limb, servo)
+# connect_wheels_to_housing(servo)
+arm = build_full_arm()
+show(arm, render_joints=True)
 # export_step(build_limb().part, "limb.step")
 # show(build_test_arm(), render_joints=True)
 # export_step(build_carriage(1.01, 2).part, 'carriage_test.step')
-export_step(limb, 'limb.step')
+# export_step(limb, 'limb.step')
